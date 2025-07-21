9906b8143fb0f1d3d26f3b04564ad02a05eabe48
bienps24 <bienps24@gmail.com>
2025-07-22 02:02:17 +0800

    Update db.js

--- db.js
+++ db.js
@@ -1,11 +1,6 @@
-const { Pool } = require('pg');
-require('dotenv').config();
-
 const pool = new Pool({
   // Palitan ang POSTGRES_URL sa DATABASE_URL
   connectionString: process.env.DATABASE_URL, 
   max: 10,
   idleTimeoutMillis: 30000
 });
-
-module.exports = pool;
